e0d45394284ce5231708c8cce1f1443613e378e5
VORDIUM <Vordium@gmail.com>
2025-05-16 12:44:26 +0400

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -109,7 +109,7 @@ app.use((req, res, next) => {
 
 // CORS configuration
 const corsOptions = {
-  origin: ["https://api.homesluxera.com"], // Allow requests from your frontend domain
+  origin: ["https://api.homesluxera.com"], // Allow requests from your custom domain
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true, // Allow cookies or authorization headers
